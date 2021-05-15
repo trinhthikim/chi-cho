56c741fff41193f23f9a4be0f2eda3f00f52b656
ngoclinhpn24 <linhpn2402@gmail.com>
2021-05-15 17:29:23 +0700

    linh update 3

--- app/src/main/java/com/example/mainscreen2/LoginActivity.java
+++ app/src/main/java/com/example/mainscreen2/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.mainscreen2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +15,13 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class LoginActivity extends AppCompatActivity{
 
-    private EditText name, password, passError;
+    private EditText name, password_1;
     private Button login;
     private TextView create_acc;
-    private boolean isPasswordValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,36 +29,78 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.activity_login_form);
 
         name = (EditText) findViewById(R.id.edt_name);
-        password = (EditText) findViewById(R.id.edt_pass);
+        password_1 = (EditText) findViewById(R.id.edt_pass);
         login = (Button) findViewById(R.id.btn_logIn);
         create_acc = (TextView) findViewById(R.id.tv_create);
 
-//        passError = (TextInputLayout) findViewById(R.id.passError);
-
         login.setOnClickListener(new View.OnClickListener() {
-
-            // public void onClick(View v) {
-//                SetValidation();
-//            }
             @Override
             public void onClick(View v) {
-                // chuyen den Trang chu
-                Intent intent1 = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent1);
+
+                String username, password;
+                username = String.valueOf(name.getText());
+                password = String.valueOf(password_1.getText());
+
+                if(!username.equals("") && !password.equals("")) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            //Creating array for data
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.1.8/LoginRegister/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Login Success")) {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                        // chuyen den Trang chu
+                                        Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent1);
+//                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // chuyen den Trang chu
+//                Intent intent1 = new Intent(LoginActivity.this, MainActivity.class);
+//                startActivity(intent1);
+//            }
+//        });
 
         create_acc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // chuyen den Trang Dang Ky
-                Intent intent2 = new Intent(LoginActivity.this, SignupActivity.class);
+                Intent intent2 = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivity(intent2);
             }
         });
     }
 
-    public void SetValidation() {
+   public void SetValidation() {
         // Check for a valid password.
 //        if (password.getText().toString().isEmpty()) {
 //            passError.setError(getResources().getString(R.string.password_error));
@@ -68,12 +112,9 @@ public class LoginActivity extends AppCompatActivity{
 //            isPasswordValid = true;
 //            passError.setErrorEnabled(false);
 //        }
-//
 //        if (isPasswordValid) {
 //            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
-//
 //        }
-
     }
 
 }
--- app/src/main/java/com/example/mainscreen2/SignupActivity.java
+++ app/src/main/java/com/example/mainscreen2/SignupActivity.java
@@ -3,20 +3,24 @@ package com.example.mainscreen2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText name, email, password, passError;
+    private EditText name, email_1, password_1;
     private Button register;
     private TextView login_acc;
-    private boolean isPasswordValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +28,73 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup_form);
 
         name = (EditText) findViewById(R.id.edt_name);
-        email = (EditText) findViewById(R.id.edt_email);
-        password = (EditText) findViewById(R.id.edt_pass);
+        email_1 = (EditText) findViewById(R.id.edt_email);
+        password_1 = (EditText) findViewById(R.id.edt_pass);
         register = (Button) findViewById(R.id.btn_register);
         login_acc = (TextView) findViewById(R.id.tv_login);
 
         register.setOnClickListener(new View.OnClickListener() {
-
-            // public void onClick(View v) {
-//                SetValidation();
-//            }
             @Override
             public void onClick(View v) {
-                // chuyen den Trang đăng ký
-                Intent intent1 = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent1);
+
+                String username, email, password;
+                username = String.valueOf(name.getText());
+                email = String.valueOf(email_1.getText());
+                password = String.valueOf(password_1.getText());
+
+                if(!username.equals("") && !password.equals("") && !email.equals("")) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[3];
+                            field[0] = "username";
+                            field[1] = "email";
+                            field[2] = "password";
+                            //Creating array for data
+                            String[] data = new String[3];
+                            data[0] = username;
+                            data[1] = email;
+                            data[2] = password;
+                            PutData putData = new PutData("http://192.168.1.8/LoginRegister/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Sign Up Success")) {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                        // chuyen den Trang đăng ký
+                                        Intent intent1 = new Intent(getApplicationContext(), LoginActivity.class);
+                                        startActivity(intent1);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
 
         login_acc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // chuyen den Trang Dang Ky
-                Intent intent2 = new Intent(SignupActivity.this, LoginActivity.class);
+                Intent intent2 = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent2);
             }
         });
     }
+
+
+
 }
